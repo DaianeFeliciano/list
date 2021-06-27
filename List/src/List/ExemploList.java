d82f9bc10f2e1477f8ac8f691a1dd81e01916ab5
Daiane Feliciano <77757301+DaianeFeliciano@users.noreply.github.com>
2021-06-26 21:28:09 -0300

    importando list

--- List/src/List/ExemploList.java
+++ List/src/List/ExemploList.java
@@ -1,5 +1,12 @@
 package List;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExemploList {
-    
+    public static void main(String[] args) {
+      List notas = new ArrayList();
+
+      
+    }
 }
