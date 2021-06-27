d7488623afbe10bfbe1c7bd679118bbf1035a168
Daiane Feliciano <77757301+DaianeFeliciano@users.noreply.github.com>
2021-06-26 21:44:08 -0300

    lists

--- List/src/List/ExemploList.java
+++ List/src/List/ExemploList.java
@@ -10,9 +10,17 @@ public class ExemploList {
 
      // List notas = new ArrayList(); antes do java 5
 
-        //    List<Double> notas = new ArrayList<>(); // list que contém elementos do tipo double/ diamond operator
+        // List<Double> notas = new ArrayList<>(); // list que contém elementos do tipo double/ diamond operator
 
      //ArrayList<Double> notas = new ArrayList<>(); // outra forma de imprementação - algumas situações recomendado
+
+    /*List<Double> notas = Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6); adicionar e remover nao pode fazer
+        notas.add(10d);
+        System.out.println(notas);*/
+        /*List<Double> notas = List.of(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6); tornar imutavel a lista -adc nem remover
+        notas.add(1d);
+        notas.remove(5d);
+        System.out.println(notas);*/
 
 
 
