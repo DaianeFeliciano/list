565f117ac29a31267f380bb0a60057befbe169fd
Daiane Feliciano <77757301+DaianeFeliciano@users.noreply.github.com>
2021-06-27 14:53:58 -0300

    paradigma funcional

--- List/src/List/Funcional.java
+++ List/src/List/Funcional.java
@@ -1,4 +1,14 @@
 package List;
 
+import java.util.function.UnaryOperator;
+
 public class Funcional {
+    public static void main(String[] args) {
+
+        UnaryOperator<Integer> calcularValorVezesTrinta = valor -> valor * 3; // conceito de paradigma funcional
+        int valor = 10;
+        System.out.println("O resultado é: " + calcularValorVezesTrinta.apply(10));
+
+    }
 }
+
