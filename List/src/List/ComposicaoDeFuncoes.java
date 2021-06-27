0483a3d4f7fc8c5eebd75655fefe40eb32947361
Daiane Feliciano <77757301+DaianeFeliciano@users.noreply.github.com>
2021-06-27 15:44:53 -0300

    conceito de imutabilidade

--- List/src/List/ComposicaoDeFuncoes.java
+++ List/src/List/ComposicaoDeFuncoes.java
@@ -7,11 +7,14 @@ public class ComposicaoDeFuncoes {
 
       int [] valores = {1,2,3,4};
 //
-//        Arrays.stream(valores)
-//                .filter(numero -> numero % 2 == 0)
-//                .map(numero -> numero * 2)
-//                .forEach(numero -> System.out.println(numero));
+       /* FORMA FUNCIONAL*/
 
+//        Arrays.stream(valores)
+//                .filter(numero -> numero % 2 == 0) // VERIFICA PARA CADA NUMERO SE O RESTO DA DIV ==0
+//                .map(numero -> numero * 2) // SE FOR MULTIPLICA POR 2
+//                .forEach(numero -> System.out.println(numero)); // MOSTRA
+
+                    /* FORMA IMPERATIVA*/
         for (int i = 0; i < valores.length ; i++) {
             int valor = 0;
             if(valores[i]  % 2 == 0) {
