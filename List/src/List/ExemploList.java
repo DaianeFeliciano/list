0eae08cd7b3c4fdd2dde2a2bc8cdb0667e088ff3
Daiane Feliciano <77757301+DaianeFeliciano@users.noreply.github.com>
2021-06-26 21:38:08 -0300

    formas de implementar o list

--- List/src/List/ExemploList.java
+++ List/src/List/ExemploList.java
@@ -6,11 +6,13 @@ import java.util.List;
 public class ExemploList {
     public static void main(String[] args) {
 
+        /*FORMAS DE INICIAR O LIST*/
+
      // List notas = new ArrayList(); antes do java 5
 
-        List<Double> notas = new ArrayList<>(); // list que contém elementos do tipo double/ diamond operator
+        //    List<Double> notas = new ArrayList<>(); // list que contém elementos do tipo double/ diamond operator
 
-        
+     //ArrayList<Double> notas = new ArrayList<>(); // outra forma de imprementação - algumas situações recomendado
 
 
 
