ca4eaf3d45c7caa5be0c29eabb6ce47926fbb004
Daiane Feliciano <77757301+DaianeFeliciano@users.noreply.github.com>
2021-06-26 21:33:41 -0300

    diamond operator

--- List/src/List/ExemploList.java
+++ List/src/List/ExemploList.java
@@ -5,7 +5,12 @@ import java.util.List;
 
 public class ExemploList {
     public static void main(String[] args) {
-     // List notas = new ArrayList(); ante do java 5
+
+     // List notas = new ArrayList(); antes do java 5
+
+        List<Double> notas = new ArrayList<>(); // list que contém elementos do tipo double/ diamond operator
+
+        
 
 
 
